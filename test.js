b6269a1faafe5ac5fb7c4208145d24e2068a61ae
Lenn106 <70281701+Lenn106@users.noreply.github.com>
2023-11-24 19:38:49 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 //  this is a webhook test thing ik yall guys are gonna spam it so im deleting the webhook  after tis
-javascript:(function() {
+function send() {
     var url = 'https://discord.com/api/webhooks/1177779360497946705/SnV55qlUgwgEOqHP1KX8uqAHN7FL3lSiVab2daxyZsGY6UiCafTrnbVNS5iCOsSsS7ui';
     var message = 'csp test'; 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify({ content: message }));
-})();
+}
